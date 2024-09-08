dbbd7902a5ab4be450ae3a98839f61dbfc81559c
replakcan <alper.makcan@gmail.com>
2024-09-08 11:21:15 +0300

    Update App.jsx with Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import workintech from '/workintech.svg'
 import './App.css'
+import Header from './assets/components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Header />
     </>
   )
 }
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,8 +1,11 @@
+import React from "react"
+
 export default function () {
 
 
     return (
         <>
+        <img src="Assets/Iteration-1-assets/logo.svg" alt="teknolojik yemekler başlığı" />
         </>
     )
 }
