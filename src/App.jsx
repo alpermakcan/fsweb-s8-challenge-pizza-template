353cf6732c807082225a0fa9713e062880029948
replakcan <alper.makcan@gmail.com>
2024-09-08 12:04:09 +0300

    Update App with radio button

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
-import './App.css'
-import Header from './assets/components/Header'
-import Content from './assets/components/Content'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import workintech from "/workintech.svg";
+import "./App.css";
+import Header from "./assets/components/Header";
+import Content from "./assets/components/Content";
+
+const initialSiparis = {
+  boyut: "",
+  hamur: "",
+  ek_malzeme: "",
+  siparis_notu: "",
+  adet: "",
+  fiyat: "",
+};
+
+const pizza_boyut = ["küçük", "orta", "büyük"];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [siparis, setSiparis] = useState(initialSiparis);
 
   return (
     <>
-     <Header />
-     <Content />
+      <Header />
+      <Content />
+      <div>
+        <h3>Boyut seç</h3>
+        {
+          pizza_boyut.map((element, index) => {
+            return <label>
+          <input
+            key={index}
+            type="radio"
+            name="boyut"
+            value={element}
+            checked={siparis.boyut == {element}}
+          />
+          {element}
+        </label>
+          })
+        }
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
