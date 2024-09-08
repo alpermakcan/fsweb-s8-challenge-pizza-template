613df52e61912ca18e88b610bdf50382ba5fc5a3
replakcan <alper.makcan@gmail.com>
2024-09-08 15:45:15 +0300

    Update App.jsx with component Ekmalzemeler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import workintech from "/workintech.svg";
 import "./App.css";
 import Header from "./assets/components/Header";
 import Content from "./assets/components/Content";
+import { FormGroup, Input, Label } from "reactstrap";
 
+const ekMalzemeler = [
+  "Pepperoni",
+  "Sosis",
+  "Kanada Jambonu",
+  "Tavuk Izgara",
+  "Soğan",
+  "Domates",
+  "Mısır",
+  "Sucuk",
+  "Jalepeno",
+  "Sarımsak",
+  "Biber",
+  "Sucuk",
+  "Ananas",
+  "Kabak",
+];
 const initialSiparis = {
   boyut: "",
   hamur: "",
@@ -14,31 +29,100 @@ const initialSiparis = {
   fiyat: "",
 };
 
+const initialErrors = {
+  boyut: "",
+  hamur: "",
+};
+
+const errorMessages = {
+  boyut: "Lütfen pizza boyutu seçiniz",
+  hamur: "Lütfen hamur tipi seçiniz",
+};
+
 const pizza_boyut = ["küçük", "orta", "büyük"];
 
 function App() {
   const [siparis, setSiparis] = useState(initialSiparis);
+  const [errors, setErrors] = useState(initialErrors);
+  const [isValid, setIsValid] = useState(false);
 
+  function handleInputChange(event) {
+    let { name, value, type, checked } = event.target;
+
+    if (name === "boyut") {
+      setSiparis({ ...siparis, [name]: value });
+    }
+    console.log(siparis);
+  }
   return (
     <>
       <Header />
       <Content />
-      <div>
-        <h3>Boyut seç</h3>
-        {
-          pizza_boyut.map((element, index) => {
-            return <label>
-          <input
-            key={index}
-            type="radio"
-            name="boyut"
-            value={element}
-            checked={siparis.boyut == {element}}
-          />
-          {element}
-        </label>
-          })
-        }
+      <div className="boyut-hamur-area">
+        <div className="multiple-area">
+          <h3>Departman seçiniz:</h3>
+          <label>
+            <input
+              type="radio"
+              name="boyut"
+              value="küçük"
+              onChange={handleInputChange}
+              checked={siparis.boyut === "küçük"}
+            />{" "}
+            Küçük
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="boyut"
+              value="orta"
+              onChange={handleInputChange}
+              checked={siparis.boyut === "orta"}
+            />{" "}
+            Orta
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="boyut"
+              value="büyük"
+              onChange={handleInputChange}
+              checked={siparis.boyut === "büyük"}
+            />{" "}
+            Büyük
+          </label>
+          {errors.boyut && <p className="error-message">{errors.boyut}</p>}
+        </div>
+        <div className="multiple-area">
+          <label className="bold-label">
+            Hamur Seç
+            <select
+              value={siparis.hamur}
+              name="hamur"
+              onChange={handleInputChange}
+            >
+              <option value="İnce">İnce</option>
+              <option value="Orta">Orta</option>
+              <option value="Kalın">Kalın</option>
+            </select>
+          </label>
+        </div>
+      </div>
+      <div className="ek-malzeme-area">
+        {ekMalzemeler.map((malzeme, index) => {
+          return (
+            <label key={index}>
+              <input
+                type="radio"
+                name="ek-malzeme"
+                value={malzeme}
+                onChange={handleInputChange}
+                checked={siparis.ek_malzeme === malzeme}
+              />{" "}
+              {malzeme}
+            </label>
+          );
+        })}
       </div>
     </>
   );
